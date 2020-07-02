042fa0cb7085507e2dc057b68f23456cba9e39e9
Opjum <krasher736@gmail.com>
2020-07-02 20:47:59 +0200

    dodana metoda 1

--- WeatherForecast.cs
+++ WeatherForecast.cs
@@ -11,5 +11,10 @@ namespace WebApplication1
         public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
 
         public string Summary { get; set; }
+        public string umbo(string uwu)
+        {
+            return "test giit";
+        }
     }
+    
 }
